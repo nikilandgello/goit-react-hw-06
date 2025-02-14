ac9025d74433f7e0bc1b2d570836b7df3e6b1c35
nikilandgello <nikoscha22012004@gmail.com>
2025-02-14 12:00:26 +0200

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { ThemeProvider } from "./context/ThemeContext";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeProvider>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </ThemeProvider>
     </Provider>
   </StrictMode>
